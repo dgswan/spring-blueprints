b6d62a2f34ddc74a88731d561a2bc0baf539c783
Dmitry Lebedko <lebedko.dmitrii@gmail.com>
2026-01-26 16:59:42 +0100

    Cleanup.

--- src/main/java/org/blueprint/springblueprints/SpringBlueprintsApplication.java
+++ src/main/java/org/blueprint/springblueprints/SpringBlueprintsApplication.java
@@ -1,9 +1,13 @@
 package org.blueprint.springblueprints;
 
+import lombok.NoArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static lombok.AccessLevel.PRIVATE;
+
 @SpringBootApplication
+@NoArgsConstructor(access = PRIVATE)
 public class SpringBlueprintsApplication {
 
     static void main(String[] args) {
