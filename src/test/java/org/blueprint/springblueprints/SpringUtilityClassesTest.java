36889f10d14cb568986fcfd73fb22331bbae5c30
Dmitry Lebedko <lebedko.dmitrii@gmail.com>
2026-01-26 16:45:07 +0100

    Unit tests with examples.

---
+++ src/test/java/org/blueprint/springblueprints/SpringUtilityClassesTest.java
@@ -0,0 +1,46 @@
+package org.blueprint.springblueprints;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.util.StreamUtils;
+
+import java.nio.charset.StandardCharsets;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SpringUtilityClassesTest {
+
+    private Resource request = new ClassPathResource("http-request-payload.json");
+
+    @Test
+    @SneakyThrows
+    void copyToString() {
+        try (final var is = request.getInputStream()) {
+            assertThat(StreamUtils.copyToString(is, StandardCharsets.UTF_8))
+                    .isNotBlank()
+                    .isEqualToIgnoringWhitespace("""
+                            {
+                              "superhero": "Homelander",
+                              "action": "Shot with laser beams from eyes"
+                            }
+                            """);
+        }
+    }
+
+    @Test
+    @SneakyThrows
+    void copyToByteArray() {
+        try (final var is = request.getInputStream()) {
+            assertThat(StreamUtils.copyToByteArray(is)).asString()
+                    .isEqualToIgnoringWhitespace("""
+                            {
+                              "superhero": "Homelander",
+                              "action": "Shot with laser beams from eyes"
+                            }
+                            """);
+        }
+    }
+
+}
