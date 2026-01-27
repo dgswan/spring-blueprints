fd374c0aa87ee6b6f87875c6e6028cd94f6c0a7f
Dmitry Lebedko <lebedko.dmitrii@gmail.com>
2026-01-27 23:17:06 +0100

    Examples for UncheckedIOException

--- src/main/java/org/blueprint/springblueprints/service/impl/AzureCdnServiceImpl.java
+++ src/main/java/org/blueprint/springblueprints/service/impl/AzureCdnServiceImpl.java
@@ -1,12 +1,16 @@
 package org.blueprint.springblueprints.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.blueprint.springblueprints.annotations.ConditionalOnEnabledCdnProvider;
 import org.blueprint.springblueprints.annotations.GDPRCompliantCdn;
 import org.blueprint.springblueprints.service.CdnService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 
 @Service
@@ -21,7 +25,13 @@ class AzureCdnServiceImpl implements CdnService {
     @Override
     public URI upload(File file) {
         log.info("Upload the file {} to Azure CDN", file.getName());
-        return URI.create("https://spring-blueprints-bucket.azure.com/" + file.getName());
+        try {
+            val content = FileCopyUtils.copyToByteArray(file);
+            log.info("{} bytes uploaded.", content.length);
+            return URI.create("https://spring-blueprints-bucket.azure.com/" + file.getName());
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
     }
 
 }
--- src/main/java/org/blueprint/springblueprints/service/impl/TencentCdnServiceImpl.java
+++ src/main/java/org/blueprint/springblueprints/service/impl/TencentCdnServiceImpl.java
@@ -1,11 +1,15 @@
 package org.blueprint.springblueprints.service.impl;
 
 import lombok.extern.slf4j.Slf4j;
+import lombok.val;
 import org.blueprint.springblueprints.annotations.ConditionalOnEnabledCdnProvider;
 import org.blueprint.springblueprints.service.CdnService;
 import org.springframework.stereotype.Service;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 import java.net.URI;
 
 @Service
@@ -19,6 +23,12 @@ class TencentCdnServiceImpl implements CdnService {
     @Override
     public URI upload(File file) {
         log.info("Upload the file {} to Tencent CDN", file.getName());
-        return URI.create("https://spring-blueprints-bucket.tencent.com/" + file.getName());
+        try {
+            val content = FileCopyUtils.copyToByteArray(file);
+            log.info("{} bytes uploaded.", content.length);
+            return URI.create("https://spring-blueprints-bucket.tencent.com/" + file.getName());
+        } catch (IOException ex) {
+            throw new UncheckedIOException(ex);
+        }
     }
 }
